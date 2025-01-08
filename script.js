5b97b755eb07142b825f53d21ec5b550e48ef840
mcthexton <mcthexton@gmail.com>
2025-01-07 20:52:28 -0800

    Revised createGrid function
    createGrid was only working correctly with number 16
    createGrid now generates an equal number of squares in the row and column

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 function createGrid(number) {
     const gridContainer = document.querySelector("#grid-container")
-    for (let i = 1; i <= number*number; i++) {
-        let div = document.createElement("div")
-        div.classList.add("square")
-        gridContainer.appendChild(div)
+    for (let i = 0; i < number; i++) {
+        let row = document.createElement("div")
+        row.classList.add("row")
+        gridContainer.appendChild(row)
+        for (let j = 1; j <= number; j++) {
+            let column = document.createElement("div")
+            column.classList.add("column")
+            row.appendChild(column)
+        }
 
         /* change div color once hovered over */
-        div.addEventListener("mouseover", (event) => {
+        row.addEventListener("mouseover", (event) => {
             event.target.style.backgroundColor = "black"
         })
-
     }
 }
 
 createGrid(16)
+
+function generateGrid(number) {
+    const gridBtn = document.getElementById("gridBtn")
+
+    /*popup for grid generate */
+    gridBtn.addEventListener("click", function () {
+    let userInput = prompt("Enter a number of squares per side (Max: 100)") 
+    if (userInput > 100) {
+        alert("You can't go higher than 100")
+        return
+    }
+    })
+}
+
+generateGrid()
+
+/* started to work with generate grid button & realized I needed to perfect my create Grid. */
+
+/*container.replaceChildren() before the loop*/
